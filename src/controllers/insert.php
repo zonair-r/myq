450084062c400ca4c780de6c5af3ef9a46d5cffb
Ali Raja <zonair.raj@gmail.com>
2019-07-16 20:38:46 +0500

    Fix Media Upload without Login

--- src/controllers/insert.php
+++ src/controllers/insert.php
@@ -12,18 +12,23 @@ function test_input($data) {
 $q_text = $msg = $cats = "";
 
 if (isset($_POST['quoteText'])) {
-  $user_id = $_SESSION['id'];
-  $q_text = test_input($_POST["quoteText"]);
-  $msg = test_input($_POST["msg"]);
+  if (!isset($_SESSION['id'])){
+      echo "Please Login First to Upload Media";
+  } else {
+    $user_id = $_SESSION['id'];
+    $q_text = test_input($_POST["quoteText"]);
+    $msg = test_input($_POST["msg"]);
+  
+  $query ="INSERT INTO `quote_table`(`user_id`,`q_text`, `msg`) VALUES ('$user_id','$q_text','$msg')";
+  
+  
+  if(mysqli_query($conn, $query)){
+    echo "Quote Entered";
+  } else {
+    echo "Quote Not Entered" . $q_text . $msg;
+  }
+  }
 
-$query ="INSERT INTO `quote_table`(`user_id`,`q_text`, `msg`) VALUES ('$user_id','$q_text','$msg')";
-
-
-if(mysqli_query($conn, $query)){
-  echo "Quote Entered";
-} else {
-  echo "Quote Not Entered" . $q_text . $msg;
-}
 
 }
 ?>
