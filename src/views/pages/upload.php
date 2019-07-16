2375847fab58770be7e91b3aa922c9eeb186f044
Ali Raja <zonair.raj@gmail.com>
2019-07-17 00:00:49 +0500

    unique name store

--- src/views/pages/upload.php
+++ src/views/pages/upload.php
@@ -5,11 +5,14 @@ $storeFolder = 'upload/';   //2
  
 if (!empty($_FILES)) {
      
-    $tempFile = $_FILES['file']['tmp_name'];          //3             
-      
+    $tempFile = $_FILES['file']['tmp_name'];     
+         //3             
+    $strtotime = strtotime("now");
+    $tryName = $strtotime.'_'.$_FILES['file']['name'];  
+
     $targetPath = dirname( __FILE__ ) . $ds. $storeFolder . $ds;  //4
-     
-    $targetFile =  $targetPath. $_FILES['file']['name'];  //5
+    $targetFile = $targetPath . $tryName;
+    // $targetFile =  $targetPath. $_FILES['file']['name'];  //5
  
     move_uploaded_file($tempFile,$targetFile); //6
      
